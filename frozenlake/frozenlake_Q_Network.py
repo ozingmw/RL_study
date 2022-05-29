0df75b61076f49480d8b902a8fd5fccb0fc3b29b
Jonghun Shin <ozingmw@naver.com>
2022-05-29 23:22:55 +0900

    fix DQN / need predict status bar

--- frozenlake/frozenlake_Q_Network.py
+++ frozenlake/frozenlake_Q_Network.py
@@ -1,7 +1,47 @@
+from collections import deque
 import gym
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+
+
+def epsilon_greedy_policy(state, epsilon=0):
+    if np.random.rand() < epsilon:
+        return np.random.randint(output_size)
+    else:
+        Q_value = model.predict(tf.one_hot([state], input_size))
+        return np.argmax(Q_value)
+
+def sample_experiences(batch_size):
+    indices = np.random.randint(len(replay_memory), size=batch_size)
+    batch = [replay_memory[index] for index in indices]
+    states, actions, rewards, next_states, dones = [
+        np.array(
+            [experience[field_index] for experience in batch]
+        ) for field_index in range(5)
+    ]
+    return states, actions, rewards, next_states, dones
+
+def play_one_step(env, state, epsilon):
+    action = epsilon_greedy_policy(state, epsilon)
+    next_state, reward, done, info = env.step(action)
+    replay_memory.append((state, action, reward, next_state, done))
+    return next_state, reward, done, info
+
+def training_step(batch_size):
+    experiences = sample_experiences(batch_size)
+    states, actions, rewards, next_states, dones = experiences
+    next_Q_values = model.predict(tf.one_hot(next_states, input_size))
+    max_next_Q_values = np.max(next_Q_values, axis=1)
+    target_Q_values = (rewards + (1 - dones) * discount_rate * max_next_Q_values)
+    target_Q_values = target_Q_values.reshape(-1, 1)
+    mask = tf.one_hot(actions, output_size)
+    with tf.GradientTape() as tape:
+        all_Q_values = model(tf.one_hot(states, input_size))
+        Q_values = tf.reduce_sum(all_Q_values * mask, axis=1, keepdims=True)
+        loss = tf.reduce_mean(loss_fn(target_Q_values, Q_values))
+    grads = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
 
 env = gym.make("FrozenLake-v1")
@@ -9,38 +49,31 @@ env = gym.make("FrozenLake-v1")
 input_size = env.observation_space.n
 output_size = env.action_space.n
 
-learning_rate = 0.1
-
 model = keras.models.Sequential([
     keras.layers.Dense(3, activation="relu", input_shape=[input_size]),
     keras.layers.Dense(4, activation="relu"),
     keras.layers.Dense(4, activation="relu"),
-    keras.layers.Dense(output_size),
+    keras.layers.Dense(output_size, activation="softmax"),
 ])
 
+episodes = 1000
+batch_size = 32
+discount_rate = 0.95
+optimizer = keras.optimizers.Adam(learning_rate=1e-2)
+loss_fn = keras.losses.mean_squared_error
+replay_memory = deque(maxlen=10000)
 
-# X = tf.placeholder(shape=[1, input_size], dtype=tf.float32)
-# w = tf.Variable(tf.random.uniform([input_size, output_size], 0, 0.01))
-# y_pred = tf.matmul(X, w)
-# y = tf.placeholder(shape=[1, output_size], dtype=tf.float32)
-# loss = tf.reduce_sum(tf.square(y - y_pred))
-# train = tf.train
+rewards = 0
 
 
-for episode in episodes:
-    env.reset()
+for episode in range(episodes):
+    obs = env.reset()    
+    epsilon = max(1 - episode / (episodes*3/4.), 0.01)
     while True:
-        action = env.action_space.sample()
-        obs, reward, done, info = env.step(action)
+        obs, reward, done, info = play_one_step(env, obs, epsilon)
+        rewards += reward
         if done:
             break
-    train_step(inputs)
-
-@tf.function
-def train_step(inputs):
-    batch_data, labels = inputs
-    with tf.GradientTape() as tape:
-        predictions = model(batch_data, training=True)
-        loss = keras.losses.CategoricalCrossentropy(reduction=keras.losses.Reduction.NONE)(labels, predictions)
-    gradients = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    print(f"\rEpisode: {episode+1} / {episodes}, eps: {epsilon:.3f}, reward: {rewards}", end="")
+    if episode > 50:
+        training_step(batch_size)
