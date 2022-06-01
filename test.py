8a571fd30605096f738ebf9a77aad3b8bc436ca9
Jonghun Shin <ozingmw@naver.com>
2022-06-01 18:46:16 +0900

    test

--- test.py
+++ test.py
@@ -3,18 +3,5 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
-
-env = gym.make("FrozenLake-v1")
-
-obs = env.reset()
-
-print(tf.one_hot(np.array([0,1,2,3,4,5,6]), 16))
-
-model = keras.models.Sequential([
-    keras.layers.Dense(3, activation="relu", input_shape=[16]),
-    keras.layers.Dense(4, activation="relu"),
-    keras.layers.Dense(4, activation="relu"),
-    keras.layers.Dense(4, activation="softmax"),
-])
-
-model.predict(np.zeros([1, 16]))
+a = tf.constant([1,1,13,23,23,14,123,4,1])
+print(a.shape)
