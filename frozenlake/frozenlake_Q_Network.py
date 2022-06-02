21f6b8a0db8589a759e7c82d3739995e60bec8b7
Jonghun Shin <ozingmw@naver.com>
2022-06-02 22:46:00 +0900

    main_model weight error

--- frozenlake/frozenlake_Q_Network.py
+++ frozenlake/frozenlake_Q_Network.py
@@ -10,9 +10,9 @@ def epsilon_greedy_policy(state, epsilon=0):
     if np.random.rand() < epsilon:
         return np.random.randint(output_size)
     else:
-        Q_value = main_model.predict(tf.one_hot([state], input_size), verbose=0)
-        # return np.argmax(Q_value)
-        return np.random.choice(output_size, 1, p=Q_value[0])[0]
+        Q_value = main_model.predict(np.array(state).reshape(-1, 1), verbose=0)
+        return np.argmax(Q_value)
+        # return np.random.choice(output_size, 1, p=Q_value[0])[0]
 
 def sample_experiences(batch_size):
     indices = np.random.randint(len(replay_memory), size=batch_size)
@@ -27,21 +27,25 @@ def sample_experiences(batch_size):
 def play_one_step(env, state, epsilon):
     action = epsilon_greedy_policy(state, epsilon)
     next_state, reward, done, info = env.step(action)
+    if next_state == state:
+        reward -= 0.007
     if done and (reward == 0):
-        reward -= 0.1
+        reward -= 0.01
+    elif not done:
+        reward += 0.002
     replay_memory.append((state, action, reward, next_state, done))
     return next_state, reward, done, info
 
 def training_step(batch_size):
     experiences = sample_experiences(batch_size)
     states, actions, rewards, next_states, dones = experiences
-    next_Q_values = target_model.predict(tf.one_hot(next_states, input_size), verbose=0)
+    next_Q_values = target_model.predict(next_states.reshape(-1, 1), verbose=0).reshape(-1, 4)
     max_next_Q_values = np.max(next_Q_values, axis=1)
     target_Q_values = (rewards + (1 - dones) * discount_rate * max_next_Q_values)
     target_Q_values = target_Q_values.reshape(-1, 1)
     mask = tf.one_hot(actions, output_size)
     with tf.GradientTape() as tape:
-        all_Q_values = main_model(tf.one_hot(states, input_size))
+        all_Q_values = tf.reshape(main_model(states.reshape(-1, 1)), [-1, output_size])
         Q_values = tf.reduce_sum(all_Q_values * mask, axis=1, keepdims=True)
         loss = tf.reduce_mean(loss_fn(target_Q_values, Q_values))
         # loss_list.append(loss)
@@ -57,7 +61,7 @@ def bot_render(main_model, repeat=1):
         middle_reward = 0
         while True:
             env.render()
-            action = np.argmax(main_model.predict(tf.one_hot([state], input_size), verbose=0))
+            action = np.argmax(tf.reshape(main_model.predict(np.array(state).reshape(-1, 1), verbose=0), [-1, output_size]))
             state, reward, done, info = env.step(action)
             middle_reward += reward
             if done:
@@ -73,28 +77,29 @@ output_size = env.action_space.n
 
 def make_model():
     model = keras.models.Sequential([
-        keras.layers.Dense(16, activation="relu", input_shape=[input_size]),
+        keras.layers.Dense(16, activation="relu", input_shape=[None, 1]),
         keras.layers.Dense(64, activation="relu"),
-        keras.layers.Dense(output_size, activation="softmax"),
+        keras.layers.Dense(64, activation="relu"),
+        keras.layers.Dense(output_size),
     ])
     return model
 
 main_model = make_model()
 target_model = make_model()
 
-episodes = 1000
-batch_size = 100
+episodes = 2000
+batch_size = 200
 discount_rate = 0.99
-optimizer = keras.optimizers.RMSprop(learning_rate=0.25)
-loss_fn = keras.losses.CategoricalCrossentropy()
-replay_memory = deque(maxlen=1000)
+optimizer = keras.optimizers.RMSprop(learning_rate=1e-1)
+loss_fn = keras.losses.MeanSquaredError()
+replay_memory = deque(maxlen=100000)
 
 rewards = 0
 rewards_list = []
 loss_list = []
 
 for episode in range(episodes):
-    state = env.reset()    
+    state = env.reset()
     epsilon = max(1 - episode / (episodes*2/3.), 0.01)
     while True:
         state, reward, done, info = play_one_step(env, state, epsilon)
@@ -107,7 +112,7 @@ for episode in range(episodes):
         for _ in range(int(episodes*0.05)):
             training_step(batch_size)
         target_model.set_weights(main_model.get_weights())    
-        print(main_model(tf.one_hot([range(16)], input_size)))
+        print(main_model(np.array([range(16)]))[0])
 
 # fig, axes = plt.subplots(1, 2)
 sns.lineplot(range(1, len(rewards_list)+1), rewards_list, label="reward", color="red")
@@ -115,4 +120,4 @@ sns.lineplot(range(1, len(rewards_list)+1), rewards_list, label="reward", color=
 sns.set(style="darkgrid")
 plt.show()
 
-bot_render(main_model, 10)
+bot_render(main_model, 5)
