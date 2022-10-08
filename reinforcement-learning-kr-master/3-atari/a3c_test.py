d2b797ea01dd1544d49839d32ede00743d774e56
Jonghun Shin <ozingmw@naver.com>
2022-10-08 18:41:41 +0900

    10.8

--- reinforcement-learning-kr-master/3-atari/a3c_test.py
+++ reinforcement-learning-kr-master/3-atari/a3c_test.py
@@ -14,14 +14,14 @@ from gym.wrappers.resize_observation import ResizeObservation
 
 def A3C_model(action_size, state_size):
     input = keras.layers.Input(shape=state_size)
-    conv1 = keras.layers.Conv2D(32, 8, 4, activation='relu')(input)
-    conv2 = keras.layers.Conv2D(64, 4, 2, activation='relu')(conv1)
-    conv3 = keras.layers.Conv2D(64, 3, 1, activation='relu')(conv2)
+    conv1 = keras.layers.Conv2D(32, 8, 4, activation='relu', kernel_initializer='he_normal')(input)
+    conv2 = keras.layers.Conv2D(64, 4, 2, activation='relu', kernel_initializer='he_normal')(conv1)
+    conv3 = keras.layers.Conv2D(64, 3, 1, activation='relu', kernel_initializer='he_normal')(conv2)
     flatten = keras.layers.Flatten()(conv3)
-    shared_fc = keras.layers.Dense(512, activation='relu')(flatten)
+    shared_fc = keras.layers.Dense(512, activation='relu', kernel_initializer='he_normal')(flatten)
 
-    policy = keras.layers.Dense(action_size, activation='softmax')(shared_fc)
-    value = keras.layers.Dense(1, activation='linear')(shared_fc)
+    policy = keras.layers.Dense(action_size, activation='softmax', kernel_initializer='he_normal')(shared_fc)
+    value = keras.layers.Dense(1, activation='linear', kernel_initializer='he_normal')(shared_fc)
 
     model = keras.Model(inputs=input, outputs=[policy, value])
 
@@ -29,7 +29,7 @@ def A3C_model(action_size, state_size):
 
 class A3CAgent():
     def __init__(self, env_name, action_size):
-        self._make_env(env_name)
+        self.env_name = env_name
         self.action_size = action_size
         self.state_size = (84, 84, 4)
         
@@ -39,35 +39,46 @@ class A3CAgent():
         
         self.global_model = A3C_model(self.action_size, self.state_size)
         self.optimizer = keras.optimizers.Adam(learning_rate=self.lr, clipnorm=40.)
-        
-        self.model_path = os.path.join(os.getcwd(), 'save_model')
+        # set actor/critic optimizer
+
+        # self.model_path = os.path.join(os.getcwd(), 'save_model')
 
     def train(self):
-        runners = [Runner(self.env, self.action_size, self.state_size) 
+        runners = [Runner(self.env_name, self.action_size, self.state_size,
+                          self.global_model, self.discount_factor,
+                          self.optimizer) 
                    for _ in range(self.threads)]
         for i, runner in enumerate(runners):
-            print(f'Start worker #{i}')
+            print(f'Start worker #{i+1}')
             runner.start()
 
-        while True:
-            self.global_model.save_weights(self.model_path)
-            time.sleep(10*60)
-
-    def _make_env(self, env_name):
-        self.env = GrayScaleObservation(gym.make(env_name), keep_dim=True)
-        self.env = ResizeObservation(self.env, 84)
+        # while True:
+        #     self.global_model.save_weights(self.model_path)
+        #     time.sleep(10*60)
 
 class Runner(threading.Thread):
     global_episode = 0
 
-    def __init__(self, env, action_size, state_size):
+    def __init__(self, env_name, action_size, state_size, global_model,
+                 discount_factor, optimizer):
         threading.Thread.__init__(self)
-        self.env = env
+        self._make_env(env_name)
         self.action_size = action_size
+        self.state_size = state_size
+        self.global_model = global_model
+        self.discount_factor = discount_factor
+        self.optimizer = optimizer
+
+        self.states, self.actions, self.rewards = [], [], []
+
         self.local_model = A3C_model(action_size, state_size)
         self.agent_waiting = 20
         self.t = 0
         self.t_max = 20
+
+    def _make_env(self, env_name):
+        self.env = GrayScaleObservation(gym.make(env_name), keep_dim=True)
+        self.env = ResizeObservation(self.env, 84)
 
     def get_action(self, history):
         history = np.float32(history / 255.)
@@ -77,12 +88,69 @@ class Runner(threading.Thread):
         action = np.random.choice(self.action_size, 1, p=policy)[0]
         return action
 
-    def train_model(self, ):
-        pass
+    def train_model(self, done):
+        global_params = self.global_model.trainable_variables
+        local_params = self.local_model.trainable_variables
+
+        with tf.GradientTape() as tape:
+            total_loss = self.compute_loss(done)
+        grads = tape.gradient(total_loss, local_params)
+        self.optimizer.apply_gradients(zip(grads, global_params))
+        self.local_model.set_weights(self.global_model.get_weights())
+        self.states, self.actions, self.rewards = [], [], []
+
+    def compute_loss(self, done):
+        discounted_prediction = self.discounted_prediction(self.rewards, done)
+        discounted_prediction = tf.convert_to_tensor(discounted_prediction[:, None], dtype=tf.float32)
+
+        states = np.zeros((len(self.states), 84, 84, 4))
+
+        for i in range(len(self.states)):
+            states[i] = self.states[i]
+        states = np.float32(states/255.)
+
+        policy, values = self.local_model(states)
+
+        advantages = discounted_prediction - values
+        critic_loss = 0.5 * tf.reduce_sum(tf.square(advantages))
+
+        action = tf.convert_to_tensor(self.actions, dtype=tf.float32)
+        policy_prob = tf.nn.softmax(policy)
+        action_prob = tf.reduce_sum(action * policy_prob, axis=1, keepdims=True)
+        cross_entropy = -tf.math.log(action_prob + 1e-10)
+        actor_loss = tf.reduce_sum(cross_entropy * tf.stop_gradient(advantages))
+
+        entropy = tf.reduce_sum(policy_prob * tf.math.log(policy_prob + 1e-10), axis=1)
+        entropy = tf.reduce_sum(entropy)
+        actor_loss += 0.01 * entropy
+
+        total_loss = 0.5 * critic_loss + actor_loss
+
+        return total_loss
+
+    def discounted_prediction(self, rewards, done):
+        discounted_prediction = np.zeros_like(rewards)
+        running_add = 0
+
+        if not done:
+            last_state = np.float32(self.states[-1]/255.)
+            running_add = self.local_model(last_state)[-1][0]
+
+        for t in reversed(range(0, len(rewards))):
+            running_add *= self.discount_factor + rewards[t]
+            discounted_prediction[t] = running_add
+
+        return discounted_prediction
+
+    def append_sample(self, history, action, reward):
+        self.states.append(history)
+        act = np.zeros(self.action_size)
+        act[action] = 1
+        self.actions.append(act)
+        self.rewards.append(reward)
 
     def run(self):
-        global episode
-        step_per_episode = 0
+        global episode, score_avg, score_max
         
         while episode < max_episode:
             obs = self.env.reset()
@@ -96,14 +164,17 @@ class Runner(threading.Thread):
             history = np.reshape([history], (1, 84, 84, 4))
 
             while True:
-                step_per_episode += 1
                 self.t += 1
                 
                 action = self.get_action(history)
+                # action space
+                # ['NOOP', 'FIRE', 'RIGHT', 'LEFT']
+                # 0, 1, 2 -> 0, 2, 3으로 변경
+                real_action = action + 1 if action != 0 else action
                 if dead:
                     action, dead = 1, False
 
-                obs, reward, done, info = self.env.step(action)
+                obs, reward, done, info = self.env.step(real_action)
 
                 next_state = obs
                 next_state = np.reshape([next_state], (1, 84, 84, 1))
@@ -115,6 +186,8 @@ class Runner(threading.Thread):
 
                 score += reward
                 reward = np.clip(reward, -1., 1.)
+
+                self.append_sample(history, action, reward)
 
                 if dead:
                     history = np.stack((next_state, next_state, next_state, next_state))
@@ -128,14 +201,16 @@ class Runner(threading.Thread):
 
                 if done:
                     episode += 1
-                    print(f'episode: {episode}')
+                    score_max = score if score > score_max else score_max
+                    score_avg = 0.9 * score_avg + 0.1 * score if score_avg != 0 else score
 
-                    step_per_episode = 0
+                    print(f"episode: {episode:5d} | score: {score:4.1f} | score max: {score_max:4.1f} | score avg: {score_avg:.3f}")
+
                     break
 
-global episode
-episode = 0
-max_episode = 10000
+global episode, score_max, score_avg
+episode, score_max, score_avg = 0, 0, 0
+max_episode = 10000000
 
 env_name = 'BreakoutDeterministic-v4'
 global_agent = A3CAgent(env_name, action_size=3)
