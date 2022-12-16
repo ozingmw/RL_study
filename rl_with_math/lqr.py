b941689650dcd02a5ed5ec320392241e443e884a
Jonghun Shin <ozingmw@naver.com>
2022-12-16 23:05:06 +0900

    12.16

--- rl_with_math/lqr.py
+++ rl_with_math/lqr.py
@@ -14,14 +14,57 @@ configuration = {
     
 }
 
-class DynamicsPriorGMM:
-    def __init__(self) -> None:
-        pass
+def logsum(vec, axis=0, keepdims=True):
+    maxv = np.max(vec, axis=axis, keepdims=keepdims)
+    maxv[maxv == -float('inf')] = 0
+    return np.log(np.sum(np.exp(vec-maxv), axis=axis, keepdims=keepdims)) + maxv
+
+class GMM(object):
+    def __init__(self, init_sequential=False, eigreg=False, warmstart=True):
+        self.init_sequential = init_sequential
+        self.eigreg = eigreg
+        self.warmstart = warmstart
+        self.sigma = None
+
+    def estep(self, data):
+        N, D = data.shape
+        K = self.sigma.shape[0]
+
+        logobs = -0.5 * np.ones((N, K)) * D * np.log(2*np.pi)
+        
+        for i in range(K):
+            mu, sigma = self.mu[i], self.sigma[i]
+            L = scipy.linalg.cholesky(sigma, lower=True)
+            logobs[:, i] -= np.sum(np.log(np.diag(L)))
+
+            diff = (data-mu).T
+            soln = scipy.linalg.solve_triangular(L, diff, lower=True)
+            logobs[:, i] -= 0.5 * np.sum(soln**2, axis=0)
+
+            logobs += self.logmass.T
+        
+        return logobs
+
+    def clusterwts(self, data):
+        logobs = self.estep(data)
+        logwts = logobs - logsum(logobs, axis=1)
+        return logwts.T
+
+    def inference(self, pts):
+        logwts = self.clusterwts(pts)
+
+
+class DynamicsPriorGMM(object):
+    def __init__(self):
+        self.X = None
+        self.U = None
+
+        self.gmm = GMM()
 
 
 class LQRFLMagent(object):
-    def __init__(self):
-        self.env = gym.make("Pendulum-v1", g=9.80665)
+    def __init__(self, env):
+        self.env = env
         self.state_dim = self.env.observation_space.shape[0]
         self.action_dim = self.env.action_space.shape[0]
         self. action_bound = self.env.action_space.high[0]
@@ -30,7 +73,8 @@ class LQRFLMagent(object):
 
 
 MAX_ITER = 60
-agent = LQRFLMagent()
+env = gym.make("Pendulum-v1", g=9.80665)
+agent = LQRFLMagent(env)
 
 agent.update(MAX_ITER)
 T = configuration('T')
